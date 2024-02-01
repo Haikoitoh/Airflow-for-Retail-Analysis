d225fe25aeca8b668eddfde7394fe5747ae3269d
Haikoitoh <sumeet.bb@gmail.com>
2024-02-01 12:37:15 -0600

    Added changes

---
+++ include/dbt/target/compiled/retail/models/report/report_product_invoices.sql
@@ -0,0 +1,11 @@
+-- report_product_invoices.sql
+SELECT
+  p.product_id,
+  p.stock_code,
+  p.description,
+  SUM(fi.quantity) AS total_quantity_sold
+FROM `airflow-tutorial-411421`.`retail`.`fct_invoices` fi
+JOIN `airflow-tutorial-411421`.`retail`.`dim_product` p ON fi.product_id = p.product_id
+GROUP BY p.product_id, p.stock_code, p.description
+ORDER BY total_quantity_sold DESC
+LIMIT 10
---
+++ include/dbt/target/run/retail/models/report/report_product_invoices.sql
@@ -0,0 +1,23 @@
+
+  
+    
+
+    create or replace table `airflow-tutorial-411421`.`retail`.`report_product_invoices`
+    
+    
+
+    OPTIONS()
+    as (
+      -- report_product_invoices.sql
+SELECT
+  p.product_id,
+  p.stock_code,
+  p.description,
+  SUM(fi.quantity) AS total_quantity_sold
+FROM `airflow-tutorial-411421`.`retail`.`fct_invoices` fi
+JOIN `airflow-tutorial-411421`.`retail`.`dim_product` p ON fi.product_id = p.product_id
+GROUP BY p.product_id, p.stock_code, p.description
+ORDER BY total_quantity_sold DESC
+LIMIT 10
+    );
+  
